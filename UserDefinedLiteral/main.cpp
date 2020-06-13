094a842c9060089d1e5496eb3a1268cf05d951ff
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-06-13 14:47:23 +0100

    Update main.cpp

--- UserDefinedLiteral/main.cpp
+++ UserDefinedLiteral/main.cpp
@@ -2,14 +2,19 @@
 #include <iostream>
 
 
-Temperature operator"" _C(unsigned long long cel)
+Temperature operator"" _K(long double cel)
 {
 	return Temperature(cel + 273);
 }
-
+Temperature operator"" _F(unsigned long long fahr)
+{
+	return Temperature((fahr + 459.67) * 5 / 9);
+}
 int main()
 {
-	Temperature t = 12_C;
-	std::cout << "t is: " << t.temp << " Kelvin";
+	Temperature t = 12.3_K;		// change 12.3°C to Kelvin
+	Temperature t1 = 15_F;		// change 15°C to Fahrenheit
+	std::cout << "t is: " << t.temp << " Kelvin\n";
+	std::cout << "t1 is: " << t1.temp << " Fahrenheit";
 	return 0;
 }
