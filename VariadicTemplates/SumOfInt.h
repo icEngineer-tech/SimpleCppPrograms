c8d95022ac54f08be15e9ef886f587e8b75c176b
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-06-14 14:34:18 +0100

    Update SumOfInt.h

--- VariadicTemplates/SumOfInt.h
+++ VariadicTemplates/SumOfInt.h
@@ -1,5 +1,5 @@
 /* this program will return the sum of the INTEGER passed in params, if other param than int is provided then the compiler will throw an 
-error, please don't use VS 2019 as some problem with fold expression is always not corrected */
+error, please don't use VS 2019 as some problems with fold expression are not corrected yet */
 
 template<typename P, typename...T, std::enable_if_t<(std::is_integral_v<P>&&...&& std::is_integral_v<T>), bool> = true>
 auto sum(P i, T...args)
