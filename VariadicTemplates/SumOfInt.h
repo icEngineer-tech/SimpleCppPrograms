0b896ab2a4a1593b94c8b9f97b5790cc5ce8a31d
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-06-14 11:24:46 +0100

    Create SumOfInt.h

---
+++ VariadicTemplates/SumOfInt.h
@@ -0,0 +1,8 @@
+/* this program will return the sum of the INTEGER passed in params, if other param than int is provided then the compiler will throw an 
+error */
+
+template<typename P, typename...T, std::enable_if_t<(std::is_integral_v<P>&&...&& std::is_integral_v<T>), bool> = true>
+auto sum(P i, T...args)
+{
+	return (i+(args + ...));
+}
