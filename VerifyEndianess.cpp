3fc25162f50aba0f4c6047403830fc3ac6e7f7bf
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-06-19 19:45:56 +0100

    Update VerifyEndianess.cpp

--- VerifyEndianess.cpp
+++ VerifyEndianess.cpp
@@ -6,6 +6,6 @@ int a = 1178796145;
 	int* pA = &a;
 	char* pC = reinterpret_cast<char*>(pA);
 	for (int i = 0; i < 4; i++)
-		std::cout << *pC++ << std::endl;        /*if the architecture is little endian the output will be: q \0 C F else the reverse order
-  return 0;                                   will be outputed */
+		std::cout << *pC++ << std::endl;        // if the architecture is little endian the output will be: q \0 C F else the reverse order
+  return 0;                                   		// will be outputed 
 }
